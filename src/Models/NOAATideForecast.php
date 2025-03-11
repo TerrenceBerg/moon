095122bf913a30c837d852bdd7a31c19a8e5bdbd
Sanaullah Kiani <kianisanaullah@gmail.com>
2025-03-11 21:33:52 +0500

    Update NOAATideForecast.php

--- src/Models/NOAATideForecast.php
+++ src/Models/NOAATideForecast.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class NOAATideForecast extends Model
 {
     protected $guarded = [];
-//    protected $table = 'noaa_tide_forecasts_old';
+    protected $table = 'noaa_tide_forecasts';
 }
