0e101b5c4a6a9d3f0568b9ba196d18154ba9e18b
TerrenceBerg <Terrence@976-TUNA.com>
2025-03-26 11:20:10 -0700

    remove dd

--- src/Resources/views/livewire/calendar.blade.php
+++ src/Resources/views/livewire/calendar.blade.php
@@ -1,6 +1,4 @@
 <div>
-
-    {{dd($calendarData['2025'])}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <div class="fixed-header shadow-sm">
         <div class="header-content container">
